4d0bcb9238c29ef9f8a7657a2a36c3f855509e79
Tessa <Tessa>
2017-11-09 11:22:36 +1100

    default image added if image upload is nil

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :users,
+  controllers: { registrations: 'registrations'}
+
+  # resources :sessions, :only => [:new, :create, :destroy]
+
+  resources :profiles
   resources :team_members
   resources :hackathons do
     resources :teams do
@@ -6,11 +13,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :profiles
-  devise_for :users, controllers: { registrations: 'registrations'}, :path_names => {
-    :sign_up => 'signup'
-  }
-  resources :sessions, :only => [:new, :create, :destroy]
   resources :charges
 
   resources :conversations, only: [:create] do
@@ -19,8 +21,8 @@ Rails.application.routes.draw do
     end
     resources :messages, only: [:create]
   end
-  post ':controller(/:action(/:id(.:format)))'
-  get ':controller(/:action(/:id(.:format)))'
+  # post ':controller(/:action(/:id(.:format)))'
+  # get ':controller(/:action(/:id(.:format)))'
   root to: "hackathons#index"
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
